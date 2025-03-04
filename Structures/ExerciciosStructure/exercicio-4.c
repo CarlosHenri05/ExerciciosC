ba751361004739b02b098a577eefb045626e849c
John Doe <johndoe@email.com>
2025-03-04 16:35:57 -0300

    Exercicio novo

--- Structures/ExerciciosStructure/exercicio-4.c
+++ Structures/ExerciciosStructure/exercicio-4.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct
 {
@@ -32,6 +33,39 @@ void perguntarRanking(Bandas bandas[], int size)
     }
 }
 
+void exibirGeneros(Bandas bandas[], int size)
+{
+    char style[30];
+    printf("Digite um genêro de banda: ");
+    scanf("%s", style);
+
+    for (int i = 0; i < size; i++)
+    {
+        if (strcmp(bandas[i].genero, style))
+        {
+            printf("%s", bandas[i].nome);
+        }
+    }
+}
+
+void perguntarFavoritas(Bandas bandas[], int size)
+{
+    char bandaPergunta[30];
+
+    printf("Digite o nome de uma banda e veremos se ela está nos favoritos ou não: ");
+    scanf("%s", bandaPergunta);
+
+    for (int i = 0; i < size; i++)
+    {
+        if (strcmp(bandas[i].nome, bandaPergunta) == 0)
+        {
+            printf("Sim, ela está nos favoritos");
+            return;
+        }
+    }
+    printf("A banda %s não está nos favoritos.", bandaPergunta);
+}
+
 int main()
 {
 
@@ -41,30 +75,103 @@ int main()
 
     int size = sizeof(bandas) / sizeof(bandas[0]);
 
-    int i = 0;
+    int preenchido = 0;
+    int opcao;
 
-    while (i < 5)
+    do
     {
-        printf("Fale o nome da sua banda favorita: ");
-        scanf("%s", ptr->nome);
+        printf("\n===== MENU =====\n");
+        printf("1. Preencher informações das bandas\n");
+        printf("2. Listar todas as bandas\n");
+        printf("3. Buscar por ranking\n");
+        printf("4. Buscar por gênero\n");
+        printf("5. Buscar por nome\n");
+        printf("6. Sair\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao);
 
-        printf("Digite o genêro da sua banda favorita: ");
-        scanf("%s", ptr->genero);
+        switch (opcao)
+        {
+        case 1:
+            for (size_t i = 0; i < size; i++, ptr++)
+            {
+                printf("Fale o nome da sua banda favorita: ");
+                scanf("%s", ptr->nome);
 
-        printf("Quantos integrantes a banda tem? ");
-        scanf("%d", &ptr->numIntegrantes);
+                printf("Digite o genêro da sua banda favorita: ");
+                scanf("%s", ptr->genero);
 
-        printf("Em seu ranking pessoal, qual é o rank dela?");
-        scanf("%d", &ptr->ranking);
+                printf("Quantos integrantes a banda tem? ");
+                scanf("%d", &ptr->numIntegrantes);
 
-        ptr++;
-        i++;
-    }
+                printf("Em seu ranking pessoal, qual é o rank dela?");
+                scanf("%d", &ptr->ranking);
+            }
+            preenchido = 1;
+            break;
+        case 2:
+            if (!preenchido)
+            {
+                printf("Preencha as bandas primeiro.");
+            }
+            else
+            {
+                ptr = bandas;
+                printf("Lista de bandas:\n ");
+                for (int i = 0; i < size; i++)
+                {
+                    printf("Banda: %d %s Genero: %s numIntegrantes: %d Ranking: %d",
+                           i + 1, ptr->nome, ptr->genero, ptr->numIntegrantes, ptr->ranking);
+                }
+            }
 
-    ptr = bandas;
+            break;
+        case 3:
+            if (!preenchido)
+            {
+                printf("Precisa preencher as bandas primeiro.");
+            }
+            else
+            {
+                perguntarRanking(bandas, size);
+            }
+
+            break;
+        case 4:
+            if (!preenchido)
+            {
+                printf("Precisa preencher as bandas primeiro.");
+            }
+            else
+            {
+                exibirGeneros(bandas, size);
+            }
+
+            break;
+
+        case 5:
+            if (!preenchido)
+            {
+                printf("Precisa preencher as bandas primeiro.");
+            }
+            else
+            {
+                perguntarFavoritas(bandas, size);
+            }
+
+            break;
+
+        case 6:
+            printf("Saindo do programa...");
+            break;
+        default:
+            printf("Opção inválida! Tente novamente.\n");
+            break;
+        }
+    } while (opcao != 6);
 
     printf("\nLista de Bandas:\n");
-    for (i = 0; i < 5; i++)
+    for (int i = 0; i < 5; i++)
     {
         printf("Banda %d: %s, Gênero: %s, Integrantes: %d, Ranking: %d\n",
                i + 1, ptr->nome, ptr->genero, ptr->numIntegrantes, ptr->ranking);
@@ -72,4 +179,8 @@ int main()
     }
 
     perguntarRanking(bandas, size);
+
+    exibirGeneros(bandas, size);
+
+    perguntarFavoritas(bandas, size);
 }
